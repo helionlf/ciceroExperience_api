87e60e95a8f35672774c88b46714bc78e38e6195
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2025-09-12 13:31:58 -0300

    feat(db): adicionar conexão inicial com o banco de dados

--- ciceroExperience_api/settings.py
+++ ciceroExperience_api/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+# Carrega o .env
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +80,17 @@ WSGI_APPLICATION = 'ciceroExperience_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+url = urlparse(DATABASE_URL)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': url.path[1:],       # remove a barra inicial
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
     }
 }
 
--- cicero_experience/views.py
+++ cicero_experience/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Visitantes
 
 
 def index(request):
     visitantes_list = Visitantes.objects.order_by("-data_chekin")[:5]
-    template = loader.get_template("cicero_experience/index.html")
     context = {"visitantes_list": visitantes_list}
-    return HttpResponse(template.render(context, request))
+    return render(request,"cicero_experience/index.html")
