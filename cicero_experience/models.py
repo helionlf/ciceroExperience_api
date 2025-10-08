68354ccfa3a4f19a8986cec4cfe590b100d49525
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2025-10-08 13:10:20 -0300

    refactor: reestrutura modelo de banco e formulário de cadastro

--- cicero_experience/models.py
+++ cicero_experience/models.py
@@ -5,11 +5,69 @@ class Visitantes(models.Model):
     data_chekin = models.DateTimeField(auto_now_add=True)
     data = models.DateField(auto_now_add=True)
     fingerprint = models.CharField(max_length=128)
+
     pais = models.CharField(max_length=100, blank=True, null=True)
     estado = models.CharField(max_length=100, blank=True, null=True)
     cidade = models.CharField(max_length=100, blank=True, null=True)
-    transporte = models.CharField(max_length=100, blank=True, null=True)
-    idade = models.IntegerField(blank=True, null=True)
+    
+    FAIXAS_ETARIAS = (
+        ('crianca', 'Criança (até 12 anos)'),
+        ('adolescente', 'Adolescente (13 a 17 anos)'),
+        ('jovem_adulto', 'Jovem Adulto (18 a 29 anos)'),
+        ('adulto', 'Adulto (30 a 59 anos)'),
+        ('idoso', 'Idoso (60+ anos)'),
+        ('nao_informado', 'Não Informado'),
+    )
+    faixa_etaria = models.CharField(
+        max_length=20,
+        choices=FAIXAS_ETARIAS,
+        default='nao_informado',
+        blank=True,
+        null=True
+    )
+
+    OPCOES_GRUPO = (
+        ('sim', 'Sim'),
+        ('nao', 'Não'),
+        ('desconhecido', 'Desconhecido'),
+    )
+    grupo = models.CharField(
+        max_length=20,
+        choices=OPCOES_GRUPO,
+        default='desconhecido',
+        blank=True,
+        null=True
+    )
+
+    OPCOES_GENERO = (
+        ('masculino', 'Masculino'),
+        ('feminino', 'Feminino'),
+        ('outros', 'Outros'),
+        ('nao_informado', 'Não Informar'),
+    )
+    genero = models.CharField(
+        max_length=20,
+        choices=OPCOES_GENERO,
+        default='nao_informado',
+        blank=True,
+        null=True
+    )
+
+    OPCOES_COR_RACA = (
+        ('preto', 'Preto'),
+        ('pardo', 'Pardo'),
+        ('indigena', 'Indígena'),
+        ('branco', 'Branco'),
+        ('amarelo', 'Amarelo'),
+        ('nao_informado', 'Não Informar'),
+    )
+    cor_raca = models.CharField(
+        max_length=20,
+        choices=OPCOES_COR_RACA,
+        default='nao_informado',
+        blank=True,
+        null=True
+    )
 
     class Meta:
         unique_together = ("fingerprint", "data")
--- cicero_experience/views.py
+++ cicero_experience/views.py
@@ -7,42 +7,50 @@ from .models import Visitantes
 
 def index(request):
     error_message = None
+    faixas_etarias_choices = Visitantes.FAIXAS_ETARIAS
+    grupo_ = Visitantes.OPCOES_GRUPO
+    generos = Visitantes.OPCOES_GENERO
+    cor_racas = Visitantes.OPCOES_COR_RACA
+
 
     if request.method == "POST":
         pais = request.POST.get("pais", "").strip()
         estado = request.POST.get("estado", "").strip()
         cidade = request.POST.get("cidade", "").strip()
-        transporte = request.POST.get("transporte", "").strip()
-        idade_str = request.POST.get("idade", "").strip()
+
+        faixa_etaria = request.POST.get("faixa_etaria", "").strip()
+
         fingerprint = request.POST.get("fingerprint")
+
+        grupo = request.POST.get("grupo", "").strip()
+        genero = request.POST.get("genero", "").strip()
+        cor_raca = request.POST.get("cor_raca", "").strip()
 
         if not fingerprint:
             error_message = "Não foi possível identificar o visitante."
         else:
 
             try:
-                idade = int(idade_str)
-                if idade <= 0 or idade > 150:
-                    error_message = "Idade inválida."
-                else:
-                    try:
-                        Visitantes.objects.create(
-                            fingerprint=fingerprint,
-                            pais=pais,
-                            estado=estado,
-                            cidade=cidade,
-                            transporte=transporte,
-                            idade=idade
-                        )
-                        return redirect("index")
-                    except DatabaseError:
-                        error_message = "Ocorreu um erro ao salvar no banco. Tente novamente."
-                        print("aaaaaaaaaaaa", DatabaseError)
-            except ValueError:
-                error_message = "Idade inválida. Digite apenas números."
+                Visitantes.objects.create(
+                    fingerprint=fingerprint,
+                    pais=pais,
+                    estado=estado,
+                    cidade=cidade,
+                    faixa_etaria=faixa_etaria,
+                    grupo=grupo, 
+                    genero=genero,
+                    cor_raca=cor_raca 
+                )
+                return redirect("index")
+            except DatabaseError:
+                error_message = "Ocorreu um erro ao salvar no banco. Tente novamente."
 
     visitantes_list = Visitantes.objects.order_by("-data_chekin")[:5]
     return render(request, "cicero_experience/index.html", {
         "visitantes_list": visitantes_list,
+        "faixas_etarias_choices": faixas_etarias_choices,
+        "grupo_": grupo_,
+        "generos": generos,
+        "cor_racas": cor_racas,
         "error_message": error_message
     })
