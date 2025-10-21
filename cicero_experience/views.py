b0358072404e0746827f9a14493562aaeec5b02a
Hélio Nogueira <116118382+helionlf@users.noreply.github.com>
2025-10-21 13:41:52 -0300

    feat: adiciona botão de check-in junto com envio de formulário

--- cicero_experience/views.py
+++ cicero_experience/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.db import DatabaseError
-
+from django.utils import timezone
 from .models import Visitantes
-
 from django.conf import settings
 
 
@@ -14,46 +12,76 @@ def index(request):
     generos = Visitantes.OPCOES_GENERO
     cor_racas = Visitantes.OPCOES_COR_RACA
 
+    hoje = timezone.localdate()
 
     if request.method == "POST":
-        pais = request.POST.get("pais", "").strip()
-        estado = request.POST.get("estado", "").strip()
-        cidade = request.POST.get("cidade", "").strip()
-
-        faixa_etaria = request.POST.get("faixa_etaria", "").strip()
-
+        action = request.POST.get("action")
         fingerprint = request.POST.get("fingerprint")
-
-        grupo = request.POST.get("grupo", "").strip()
-        genero = request.POST.get("genero", "").strip()
-        cor_raca = request.POST.get("cor_raca", "").strip()
 
         if not fingerprint:
             error_message = "Não foi possível identificar o visitante."
         else:
-
             try:
-                Visitantes.objects.create(
+                visitante_existente = Visitantes.objects.filter(
                     fingerprint=fingerprint,
-                    pais=pais,
-                    estado=estado,
-                    cidade=cidade,
-                    faixa_etaria=faixa_etaria,
-                    grupo=grupo, 
-                    genero=genero,
-                    cor_raca=cor_raca 
-                )
-                return redirect("index")
+                    data=hoje
+                ).first()
+
+                if action == "checkin":
+                    if visitante_existente:
+                        error_message = "Você já fez o check-in hoje."
+                    else:
+                        Visitantes.objects.create(
+                            fingerprint=fingerprint,
+                            pais="",
+                            estado="",
+                            cidade="",
+                            faixa_etaria="nao_informado",
+                            grupo="desconhecido",
+                            genero="nao_informado",
+                            cor_raca="nao_informado"
+                        )
+                        return redirect("index")
+                    
+                elif action == "form":
+                    pais = request.POST.get("pais", "").strip()
+                    estado = request.POST.get("estado", "").strip()
+                    cidade = request.POST.get("cidade", "").strip()
+                    faixa_etaria = request.POST.get("faixa_etaria", "").strip()
+                    fingerprint = request.POST.get("fingerprint")
+                    grupo = request.POST.get("grupo", "").strip()
+                    genero = request.POST.get("genero", "").strip()
+                    cor_raca = request.POST.get("cor_raca", "").strip()
+
+                    if visitante_existente:
+                        visitante_existente.pais = pais
+                        visitante_existente.estado = estado
+                        visitante_existente.cidade = cidade
+                        visitante_existente.faixa_etaria = faixa_etaria
+                        visitante_existente.grupo = grupo
+                        visitante_existente.genero = genero
+                        visitante_existente.cor_raca = cor_raca
+                        visitante_existente.save()
+                    else:
+                        Visitantes.objects.create(
+                            fingerprint=fingerprint,
+                            pais=pais,
+                            estado=estado,
+                            cidade=cidade,
+                            faixa_etaria=faixa_etaria,
+                            grupo=grupo, 
+                            genero=genero,
+                            cor_raca=cor_raca 
+                        )
+                        return redirect("index")
+                
             except DatabaseError:
                 error_message = "Ocorreu um erro ao salvar no banco. Tente novamente."
 
-    visitantes_list = Visitantes.objects.order_by("-data_chekin")[:5]
     return render(request, "cicero_experience/index.html", {
-        "visitantes_list": visitantes_list,
         "faixas_etarias_choices": faixas_etarias_choices,
         "grupo_": grupo_,
         "generos": generos,
         "cor_racas": cor_racas,
         "error_message": error_message,
-        "csc_api_key": settings.CSC_API_KEY,
     })
